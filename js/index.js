926226c44f42da289544b79e3f88ea9068779a1c
Rajeev <rajeev.kulariya1996@gmail.com>
2023-07-18 17:46:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('5|4|3|2|1|Happy|Birthday|Ribha|ღ( ´･ᴗ･` )');
+      S.UI.simulate('3|2|1|Happy|Birthday|Ribha!|ღ( ´･ᴗ･` )');
     }
 
     S.Drawing.loop(function () {
